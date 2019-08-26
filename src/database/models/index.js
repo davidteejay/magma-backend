3c4bd2a081f6572842c4d7f6df76751fdf89a454
Naimaoye <naimatoyewale@gmail.com>
2019-08-26 08:01:34 +0100

    chore(sequelize): set up sequelize models and migrations
    - install sequelize and sequelize-cli
    - setup model and migration
    - setup table in the database
    
    [Maintains #167749939]

---
+++ src/database/models/index.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import configJs from '../config/config';
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'production';
+
+const config = configJs[env];
+
+const db = {};
+
+const sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => (file.indexOf('.') !== 0)
+            && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
